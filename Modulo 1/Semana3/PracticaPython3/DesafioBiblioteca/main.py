1f822ec936c91d000ed4c2bb3c639cbce7d9b23d
darag9 <danielragaudelo@gmail.com>
2025-05-09 08:54:58 -0500

    kpex

--- Modulo 1/Semana3/PracticaPython3/DesafioBiblioteca/main.py
+++ Modulo 1/Semana3/PracticaPython3/DesafioBiblioteca/main.py
@@ -8,14 +8,14 @@ def add_books(title:str="",author:str="",genre:str="",publish_year:int=0,copies:
             print("Error: This book has already been added.")
             return books_list
         
-    books_list.append({"title":title,"author":author,"genre":genre,"publish year":publish_year,"copies":copies,"repo_price":repo_price})
+    books_list.append({"title":title,"author":author,"genre":genre,"publish_year":publish_year,"copies":copies,"repo_price":repo_price})
     print(f"The book was added!\n")
     return books_list
 
 def books_validations(books_list:list)->list:
     for book in books_list:
-        if book["publish year"] > 2024 or book["publish year"] < 1800:
-            print(f"Error: Books can't register in the date {book["publish year"]}")
+        if book["publish_year"] > 2024 or book["publish_year"] < 1800:
+            print(f"Error: Books can't register in the date {book["publish_year"]}")
             books_list.remove(book)
             return books_list
         
@@ -34,6 +34,7 @@ def search_books(title:str="")->None:
     for book in books_list:
         if book["title"].lower() == title.lower():
             print(f"The book {book["title"]} was found in the database.")
+            print(f"Title: {book["title"]}\nAuthor: {book["author"]}\nGenre: {book["genre"]}\nPublish year: {book["publish_year"]}\nNumber of copies: {book["copies"]}\nReposition price: {book["repo_price"]}")
             if book["copies"] <= 0:
                 print(f"{book["title"]} is not available at the moment.")
                 break
@@ -81,40 +82,44 @@ def remove_books(title:str="")->None:
         print(f"The book {title} was not found.")
 
 def books_report()->None:
-    suma:float = 0
+    total_value:float = 0
     for book in books_list:
-        suma = (book["copies"] * book["repo_price"]) + suma
+        total_value = (book["copies"] * book["repo_price"]) + total_value
+    print(f"The invetory value is {total_value}")
 
 def main():
     print("Welcome to the Library!")
     while True:
-        opc = int(input
-('''
--------------------------------------------------
-            Please select an option:
-            1. add books
-            2. search books
-            3. update books reposition price and copies
-            4. remove books
-            5. show all books report
-            6. exit menu
--------------------------------------------------
-'''))
-        if opc == 1:
-            books_validations(add_books(input("title: "), input("author: "),input("genre: "), int(input("published year: ")), int(input("copies: ")), float(input("reposition price: "))))
-        elif opc == 2:
-            search_books(input("Book's name: "))
-        elif opc == 3:
-            books_validations(update_book_prices(input("Book's name: "), int(input("Update Invetory of copies: ")), float(input("Update reposition price: "))))
-        elif opc == 4:
-            remove_books(input("What book would you like to remove?: "))
-        elif opc == 5:
-            books_report()
-        elif opc == 6:
-            print("See you later!")
-            break
-        else:
-            print("Error: Unknown option")
+        try:    
+            opc = int(input
+    ('''
+    -------------------------------------------------
+                Please select an option:
+                1. add books
+                2. search books
+                3. update books reposition price and copies
+                4. remove books
+                5. show all books report
+                6. exit menu
+    -------------------------------------------------
+    '''))
+            if opc == 1:
+                books_validations(add_books(input("title: "), input("author: "),input("genre: "), int(input("published year: ")), int(input("copies: ")), float(input("reposition price: "))))
+            elif opc == 2:
+                search_books(input("Book's name: "))
+            elif opc == 3:
+                books_validations(update_book_prices(input("Book's name: "), int(input("Update Invetory of copies: ")), float(input("Update reposition price: "))))
+            elif opc == 4:
+                remove_books(input("What book would you like to remove?: "))
+            elif opc == 5:
+                books_report()
+            elif opc == 6:
+                print("See you later!")
+                break
+            else:
+                print("Error: Unknown option")
+        except ValueError:
+            print("Error: The input Value is not supported. Please input a valid value.")
 
 
 main()

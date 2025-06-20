ffabea314de31a036236f73cc588a5304462e3f7
darag9 <danielragaudelo@gmail.com>
2025-06-20 12:54:34 -0500

    EntrenamientoSemana1 updated  validations

--- Modulo3/Semana1/EntrenamientoSemana1/EntrenamientoSemana1.js
+++ Modulo3/Semana1/EntrenamientoSemana1/EntrenamientoSemana1.js
@@ -7,55 +7,65 @@ let notas = [];
 
 let nombre = prompt("Input your name!");
 let edad = prompt("Input your age!");
-
-// Age validation
-if (isNaN(edad)) {
-    console.error("Invalid age input");
-} else if (edad < 0) {
-    console.error("the age must be a positive number");
-} else if (edad >= 18) {
-    alert(`Congrats ${nombre}! you're an adult with ${edad} years!`);
-} else if (edad < 18) {
-    alert(`Cheer up ${nombre}, you'll be an adult in no time!`);
-}
-
-let numNotas = prompt("How many grades are you going to add");
 edad = parseInt(edad);
 
-
-// Amount of grades validation
-if (isNaN(numNotas)) {
-    console.error("Invalid numeric input");
+// Age validation
+if (nombre == null || nombre == '') {
+    alert('please input a valid name!')
+} else if (isNaN(edad)) {
+    alert("Invalid age input");
 } else {
-    numNotas = parseInt(numNotas);
-}
-
-// Grade input and validation
-for (let i=0;i<numNotas;i++) {
-
-    let numero = prompt("Input your grade");
-
-    if (isNaN(numero)){
-        console.error("Invalid grade input");
-    } else {
-        numero = parseFloat(numero);
-        notas.push(numero);
+    
+    if (edad < 0) {
+        alert("the age must be a positive number");
+    } else if (edad >= 18) {
+        alert(`Congrats ${nombre}! you're an adult with ${edad} years!`);
+    } else if (edad < 18) {
+        alert(`Cheer up ${nombre}, you'll be an adult in no time!`);
     }
+
+    let numNotas = prompt("How many grades are you going to add");
+    console.log(typeof(numNotas));
+
+    // Amount of grades validation
+    if (isNaN(numNotas)) {
+        alert("Invalid grades quantity input");
+    } else if (numNotas < 0) {
+        alert('Grades quantity must be positive');
+    } else {
+        numNotas = parseInt(numNotas);
+    }
+
+    // Grade input and validation
+    for (let i=0;i<numNotas;i++) {
+
+        let numero = prompt("Input your grade");
+
+        if (isNaN(numero)){
+            alert("Invalid grade input");
+        } else if (numero < 0) {
+            alert('Grades must be positive');
+        } else {
+            numero = parseFloat(numero);
+            notas.push(numero);
+        }
+    }
+
+    for (let nota of notas) {
+        sum += nota;
+        cont += 1;
+    }
+
+    prom = sum / cont;
+    prom = parseFloat(prom);
+
+    // Grades validation
+
+    if (isNaN(prom)) {
+        alert("There's no grades to average");
+    } else {
+        alert(`${nombre} you have a ${prom} GPA`);
+    }
+
+
 }
-
-for (let nota of notas) {
-    sum += nota;
-    cont += 1;
-}
-
-prom = sum / cont;
-prom = parseFloat(prom);
-
-// Grades validation
-
-if (isNaN(prom)) {
-    console.error("There's no grades to average");
-} else {
-    alert(`${nombre} you have a ${prom} GPA`);
-}
-
